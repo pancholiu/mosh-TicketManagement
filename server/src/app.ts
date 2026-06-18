018aa6253018012f2b6fe7acaa2be3de3a856346
Francisco Liu <pancholiu@hotmail.com>
2026-06-18 17:52:02 -0300

    Set up Better Auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from './lib/auth'
 
 const app = express()
 
 app.use(cors())
+app.all('/api/auth/*', toNodeHandler(auth))
 app.use(express.json())
 
 app.get('/health', (_req, res) => {
